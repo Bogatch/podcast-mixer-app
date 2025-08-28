c06af3c5ef689280f5f623ecd59723782d5b891f
Marcel Bohac <marcel@Marcels-MacBook-Air.local>
2025-08-28 14:26:59 +0100

    Add Windows build workflow

---
+++ podcast-mixer-desktop/main.js
@@ -0,0 +1,31 @@
+// main.js
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // Načítaj tvoju webovú appku (Vercel)
+  win.loadURL("https://pms.customradio.sk");
+
+  // DevTools len pri ladení
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
